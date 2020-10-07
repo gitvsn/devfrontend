1785f561c4cf07df57d65fa3ae991acedf147430
Anatoliy <54946507+Antaaaaa@users.noreply.github.com>
2020-10-07 19:00:22 +0300

    add success modal

--- src/components/SuccessModal.vue
+++ src/components/SuccessModal.vue
@@ -1,32 +1,59 @@
 <template>
-  <div class="KawaiiPopup-container">
-    <div class="KawaiiPopup-bg"></div>
-    <div class="KawaiiPopup-slide">
-      <div class="KawaiiPopup">
-        <button @click="close" class="KawaiiPopup__close"></button>
-        <div class="popup-result">
-          <div class="popup-result__icon">
-            <img src="@/assets/img/success-icon.svg" alt="">
-          </div>
-          <div class="KawaiiPopup__title mt-3">
-            <p>Сhanges saved<br>
-              successfully</p>
-          </div>
-          <button type="button" class="my-btn"><span>OK</span></button>
-        </div>
-
-
-      </div>
-    </div>
-  </div>
+	<div class="KawaiiPopup-container">
+		<div class="KawaiiPopup-bg"></div>
+		<div class="KawaiiPopup-slide">
+			<div class="KawaiiPopup">
+				<button @click="closeWindow" class="KawaiiPopup__close"></button>
+				<div class="popup-result">
+					<div class="popup-result__icon">
+						<img src="@/assets/img/success-icon.svg" alt="" />
+					</div>
+					<div class="KawaiiPopup__title mt-3">
+						<p v-if="type === CHECK_MAIL">Please check your mail</p>
+						<p v-if="type === PASSWORD_CHANGED">
+							Your password has been successfully changed
+						</p>
+						<p v-if="type === DATA_SAVED">
+							Your data has been saved successfully
+						</p>
+					</div>
+					<button type="button" class="my-btn" @click="closeWindow">
+						<span>OK</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: "SuccessModal"
-    }
+const CHECK_MAIL = 'ch_m';
+const PASSWORD_CHANGED = 'ps_ch';
+const DATA_SAVED = 'dt_sv';
+export default {
+	name: 'SuccessModal',
+	methods: {
+		closeWindow() {
+			this.show = false;
+			this.$emit('close');
+		},
+	},
+	data() {
+		return {
+			show: false,
+			type: null,
+			CHECK_MAIL,
+			PASSWORD_CHANGED,
+			DATA_SAVED,
+		};
+	},
+	watch: {
+		$modalWindow(newValue, oldValue) {
+			this.type = newValue.type;
+			this.show = !this.show;
+		},
+	},
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/index';
+import store from './store';
 //Vue.config.productionTip = true;
 
+const modalWindow = Vue.observable({
+	modalWindow: {
+		type: null,
+	},
+});
+
+Object.defineProperty(Vue.prototype, '$modalWindow', {
+	get() {
+		return modalWindow.modalWindow;
+	},
+	set(value) {
+		modalWindow.modalWindow = value;
+	},
+});
+
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: {
-    App
-  }
+	el: '#app',
+	router,
+	store,
+	template: '<App/>',
+	components: {
+		App,
+	},
 });
