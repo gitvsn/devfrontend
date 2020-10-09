33939d5fb3c9973d4002a7e25c8150e4a17b2ff6
Maxim-Turovets <mturovec71@gmail.com>
2020-10-09 17:04:31 +0300

    change ip

--- src/api/config.js
+++ src/api/config.js
@@ -1,14 +1,6 @@
 import axios from 'axios';
 
 const host = window.location.hostname;
-const prodHosts = [
-  '104.248.41.44',
-  'pay.s-wallet.ai',
-];
-
-// export const baseHost = prodHosts.includes(host)
-//   ? 'https://pay.s-wallet.ai' // PROD
-//   : 'http://104.248.84.110:8080'; // DEV
 
 
 export const baseHost = 'http://localhost:8777';
