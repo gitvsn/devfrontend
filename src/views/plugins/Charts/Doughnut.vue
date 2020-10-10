623ab7f1c02bfb2dc78744a3bba09db56570f683
Maxim-Turovets <mturovec71@gmail.com>
2020-10-10 20:25:25 +0300

    percent tooltip fixed

--- src/views/plugins/Charts/Doughnut.vue
+++ src/views/plugins/Charts/Doughnut.vue
@@ -41,6 +41,13 @@ export default {
 					cutoutPercentage: 60,
 					responsive: true,
 					maintainAspectRatio: true,
+          tooltips: {
+              callbacks: {
+                label: function (tooltipItem, data) {
+                  return data['labels'][tooltipItem['index']] + ': ' + data['datasets'][0]['data'][tooltipItem['index']] + '%'
+                }
+              }
+            }
 				}
 			);
 		},
