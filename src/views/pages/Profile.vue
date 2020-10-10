16c7bb4d6dfbe8087906ceef86200d4df25a418d
Andrei Hais <andrei.gais@i.ua>
2020-10-10 14:56:47 +0300

    fix validation on profile page

--- src/views/pages/Profile.vue
+++ src/views/pages/Profile.vue
@@ -448,7 +448,7 @@ export default {
 			if (this.oldPassword && this.confirmPassword) {
 				e.preventDefault();
 
-				if (this.newPassword !== this.confirmNewPassword || this.newPassword==='' || this.confirmNewPassword === '') {
+				if (this.newPassword !== this.confirmNewPassword && (this.password ==='' && this.newPassword==='' || this.confirmNewPassword === '')) {
 					this.confirmPassword = false;
 				} else {
 					this.changePersonalInfo(userInfo)
